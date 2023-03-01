2ead12500c08a6564f4ef125d7c5919e9cdf6860
Lucas Correia <73201052+lucasctsb@users.noreply.github.com>
2023-03-01 08:51:36 -0300

    Create sql_coding.sql

---
+++ stratascratch/SQL/sql_coding.sql
@@ -0,0 +1,11 @@
+# 1 - Write a query that calculates the difference between the highest salaries found in the marketing and engineering departments. Output just the absolute difference in salaries.
+# Code:
+  SELECT ABS(
+   MAX(CASE WHEN d.department = 'engineering' THEN e.salary ELSE 0 END) - 
+   MAX(CASE WHEN d.department = 'marketing' THEN e.salary ELSE 0 END)
+) as diff_max_salaries
+FROM db_employee e
+JOIN db_dept d ON e.department_id = d.id
+WHERE d.department IN ('engineering', 'marketing')
+
+# 2 - 
